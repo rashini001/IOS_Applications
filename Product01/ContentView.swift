ca3a9aaf3cb5ab9838e58ecc353620de23cde917
COBSCCOMP24.2P-056 <cobsccomp24.2p-056@NIBMADMIN06s-iMac.local>
2026-01-10 14:21:36 +0530

    first

---
+++ Product01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Product01
+//
+//  Created by COBSCCOMP24.2P-056 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Product01/Product01App.swift
@@ -0,0 +1,17 @@
+//
+//  Product01App.swift
+//  Product01
+//
+//  Created by COBSCCOMP24.2P-056 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Product01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
